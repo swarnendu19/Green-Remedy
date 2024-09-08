44161938e68bceea7bbec94e41f5cf9b3dff05c9
Swarnendu <swarnendumaity633@gmail.com>
2024-09-08 19:16:11 +0530

    Bug fixes on production

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import { lazy } from 'react'
 import './App.css'
-import About from './components/About'
 const Tulsi = lazy(() => import("./pages/Tulsi"))
-const Home = lazy(() => import("./pages/Home"))
-
-import ThreeJs from './pages/ThreeJs'
-
+ 
 function App() {
 
   return (
