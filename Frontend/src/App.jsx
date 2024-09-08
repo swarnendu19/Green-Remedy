cb2e73f21b0729864ca9c8f39b309d403154e9e5
Debjit-Misra <debjitmisra23@gmail.com>
2024-09-08 19:59:17 +0530

    bug fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import { lazy } from 'react'
 import './App.css'
 
-// import Tulsi from './pages/Tulsi'
-// import Home from './Home'
 
-// Importing the pages
-
-const Tulsi = lazy(() => import("./pages/Tulsi"))
 const Home = lazy(() => import("./pages/home"))
 
 function App() {
